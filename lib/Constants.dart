b24db0a488f05ab3298b0dbdb00f9ed62ce45828
AMIRA999-dev <83040467+AMIRA999-dev@users.noreply.github.com>
2022-06-28 13:34:06 +0200

    Add files via upload

---
+++ lib/Constants.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/material.dart';
+    Color PrimaryColor = Color.fromRGBO(23, 20, 41, 1);
+    Color SecondaryColor = Color.fromRGBO(102, 95, 214, 1);
+    Color ThirdColor = Color.fromRGBO(243, 244, 246, 1);
